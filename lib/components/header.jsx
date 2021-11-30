683687e1397935f7bc9741ad054e9e5f920def61
Nuri Hodges <nuri@irunbackwards.com>
2021-11-29 17:52:09 -0800

    chore: initial sloppy commit

---
+++ lib/components/header.jsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/Home.module.css'
+
+export default function Header() {
+  
+    return (
+        <nav className={styles.nav}>
+            <h1 className={styles.title}>
+                NFT Explorer
+            </h1>
+        </nav>
+    )
+  }
+  
